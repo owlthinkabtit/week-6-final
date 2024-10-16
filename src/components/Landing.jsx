9a59012a9b97325be375ba7b667b4ecc0f39c01d
owlthinkabtit <haggins.jameka@gmail.com>
2024-10-16 16:06:04 -0400

    slowly working on the landing page and making sure everything is in order.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,14 +11,20 @@ const Landing = () => {
               <img className="logo-img" src={MoviePix} alt="" />
             </div>
             <div className="links">
-              <div className="nav-link">
-                <a href="/">Home</a>
-                <a href="/">Find Movies</a>
-                <a href="/">Random</a>
-              </div>
+                <a href="/" className="nav-link">Home</a>
+                <a href="/" className="nav-link">Find Movies</a>
+                <a href="/" className="nav-link">Random</a>
             </div>
           </div>
         </header>
+        <section id="landing-page">
+          <div className="content-wrapper__landing">
+            <div className="flex-col">
+              <h1 className="sub--title">Start your cinematic adventure <span className="orange">now</span></h1>
+              <h2 className="sub--title2">with the power of <span className="blue">MoviePix</span></h2>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
